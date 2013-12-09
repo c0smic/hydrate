58b385d6611787a2500bd0fbea529cef2d6f3f21
Jack Ketcham <kcajketcham@gmail.com>
2013-12-08 17:24:51 -0700

    Add routing for track page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,9 +12,25 @@ angular.module('hydrateApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/track', {
+        templateUrl: 'views/track.html',
+        controller: 'TrackCtrl'
+      })
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'aboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'contactCtrl'
       })
       .otherwise({
         redirectTo: '/'
